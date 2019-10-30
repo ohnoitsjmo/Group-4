a296eb0d7635a974af557f6d86d9a8e6e9456dbc
Arun Shriram <ashriram@calpoly.edu>
2019-10-29 18:29:40 -0700

    added basic project structure

--- webapp/src/router.js
+++ webapp/src/router.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Home from "./common/layouts/home/Home.vue";
 
 Vue.use(Router);
 
@@ -14,13 +14,13 @@ export default new Router({
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
+      path: "/shop",
+      name: "shop",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+        import(/* webpackChunkName: "about" */ "./common/layouts/shop/Shop.vue")
     }
   ]
 });
