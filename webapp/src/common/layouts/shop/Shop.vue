9605d80fcef26e446bea18be505328ab1d702a45
Arun Shriram <ashriram@calpoly.edu>
2019-11-03 21:14:44 -0800

    removed duplicate files

---
+++ webapp/src/common/layouts/shop/Shop.vue
@@ -0,0 +1,6 @@
+
+<template>
+  <div class="shop">
+    <h1>This is the shop page</h1>
+  </div>
+</template>
--- webapp/src/router.js
+++ webapp/src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./common/layouts/home/Home.vue";
+import Shop from "./common/layouts/shop/Shop.vue";
 
 Vue.use(Router);
 
@@ -19,8 +20,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./common/layouts/shop/Shop.vue")
+      component: Shop
     }
   ]
 });
