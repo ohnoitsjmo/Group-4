5ae7338a4aa48b4473e6d56ca29d066fe17f77ab
dfujiwar <46456196+dfujiwar@users.noreply.github.com>
2019-11-04 16:36:11 -0800

    update users.js

--- api/entities/user.js
+++ api/entities/user.js
@@ -1,13 +1,32 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import Orders from './orders'
 
 @Entity()
 export default class User {
   @PrimaryGeneratedColumn()
   id
 
+  @column ({ type: 'varchar', unique: true })
+  first
+
+  @column ({ type: 'varchar', unique: true })
+  last
+
   @Column({ type: 'varchar', unique: true })
   email
 
   @Column({ type: 'varchar', nullable: false })
   password
+
+  @Column({ type: 'varchar', unique: true })
+  creditcard
+  
+  @Column({ type: 'varchar', unique: true })
+  address
+
+  @Column({ type: "enum", enum: ["Admin", "Employee", "Customer"], default: "Customer"})
+  role
+
+  @OneToMany(() => Orders, (orders) => orders.customer_id,{eager:true})
+  orders
 }
