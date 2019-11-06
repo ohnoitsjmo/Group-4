6095ea299f729d71475d0b6fbe0f2a069ef80ce2
dfujiwar <46456196+dfujiwar@users.noreply.github.com>
2019-11-06 10:46:33 -0800

    Update announcement.js

--- api/routes/announcement.js
+++ api/routes/announcement.js
@@ -12,75 +12,14 @@ router.route('/announcement')
       res.send(foundAnnouncements);
     })
   })
-  /*
-  .post((req, res) => {
-    const { status, price, products } = req.body;
-    const manager = getManager();
-    const order = manager.create(Order, { status, price, products });
-    order.user = { 
-      "id": req.user.id,
-      "email": req.user.email
-    };
-    manager.save(order).then((savedOrder) => {
-      res.send(savedOrder);
-    });
-  });
-/*const router = Router();
-router.route('/announcements')
-  .all(isAuthenticated)
-  .get((req, res) => {
-    res.send(req.announcement);
-  })
-  .post((req, res) => {
-    const { text, time } = req.body;
-    const manager = getManager();
-    const announcement = manager.create(Announcement, { text, time });
-    manager.save(announcement).then((savedAnnoucement) => {
-      res.send(savedAnnoucement);
-    });
-  });
-/*const router = Router();
-router.route('/announcment')
-  .all(isAuthenticated)
-  .get((req, res) => {
-    res.send(req.announcement);
-  })
+  
   .post((req, res) => {
     const { text } = req.body;
     const manager = getManager();
-    const annoucment = manager.create(Announcement, { text});
-    announcement.time = req.time;
-    manager.save(annoucment).then((savedAnnoucement) => {
-      res.send(savedAnnoucement);
+    const announcement = manager.create(Announcement, { text});
+    manager.save(announcement).then((savedAnnouncement) => {
+      res.send(savedAnnouncement);
     });
   });
-  router.route('/annoucement/:id')
-  .get((req, res, next) => {
-    getRepository(Announcement).findOneOrFail(
-      req.params.id,
-    ).then((_foundAnnouncement) => {
-      req.category = _foundAnnouncement;
-      res.send(_foundAnnouncement);
-    }, () => {
-      res.send(404);
-    });
-  })
-  .all(isAuthenticated)
-  .put((req, res) => {
-    const foundAnnoucement = req.Annoucnement;
-    const { text} = req.body;
-    foundAnnoucement.text = text;
-    getManager().save(foundAnnoucement).then((updatedAnnoucement) => {
-      res.send(updatedAnnoucement);
-    });
-  })
-  .get((req, res) => {
-    res.send(req.annoucment);
-  })
-  .delete((req, res) => {
-    getManager().delete(Announcement, req.annoucment.id).then(() => {
-      res.send(200);
-    });
-  });*/
 
 export default router;
