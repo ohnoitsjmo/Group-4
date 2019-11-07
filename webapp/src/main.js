7013032d2ca5e2303d673589502f7419cce3aa53
Morales <K@Ks-MacBook-Air.local>
2019-11-06 20:42:17 -0800

    profileandcart

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import Vuetify from "vuetify";
-import "vuetify/dist/vuetify.css";
+import "vuetify/dist/vuetify.min.css";
 
 Vue.config.productionTip = false;
 Vue.use(Vuetify);
