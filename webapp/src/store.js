77f175acaad5261f0fd5a3e16a3c83361f0b83ac
Arun Shriram <ashriram@calpoly.edu>
2019-11-17 14:47:49 -0800

    Cart static data is now routed through models and controller

--- webapp/src/store.js
+++ webapp/src/store.js
@@ -9,6 +9,7 @@ Vue.use(Vuex)
 // and ignore the implementation details
 import { fetchItem } from "./api";
 import ShopDataModel from "@/models/shop/ShopDataModel.js";
+import CartDataModel from "@/models/cart/CartDataModel.js";
 export function createStore () {
   return new Vuex.Store({
     // IMPORTANT: state must be a function so the module can be
@@ -37,3 +38,4 @@ export function createStore () {
 }
 
 export let ShopModel = new ShopDataModel();
+export let CartModel = new CartDataModel();
